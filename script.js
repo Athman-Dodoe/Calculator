a3b80ac81833987abd9ddbd17c61322dbf9dbc6f
Athman-Dodoe <dodoeathman@gmail.com>
2023-10-16 13:18:42 +0300

    added an equal listener function
    two operate on two values first with one operator

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const nine = document.querySelector('#nine');
+const eight = document.querySelector('#eight');
+const seven = document.querySelector('#seven');
+const six = document.querySelector('#six');
+const five = document.querySelector('#five');
+const four = document.querySelector('#four');
+const three = document.querySelector('#three');
+const two = document.querySelector('#two');
+const one = document.querySelector('#one');
+const zero = document.querySelector('#zero');
+
+const quotient = document.querySelector('#divide');
+const plus = document.querySelector('#add');
+const minus = document.querySelector('#subtract');
+const product = document.querySelector('#multiply');
+const equal = document.querySelector('#equal');
+
+
+const output = document.querySelector('#output');
+const clear = document.querySelector('#clear');
+const del = document.querySelector('#del');
+
+
 let firstNum;
 let secondNum;
 let operator;
@@ -43,9 +66,118 @@ function operate(operator,firstNum, secondNum) {
     }
 }
 
-firstNum = 6;
-operator = '/';
-secondNum = 3;
 
-const result = operate(operator, firstNum, secondNum);
-console.log(`${firstNum} ${operator} ${secondNum} = ${result}`);
+
+let displayValue = '';
+
+
+function updateDisplay(newValue) {
+    displayValue += newValue;
+    output.textContent = displayValue;
+    console.log(displayValue)
+}
+
+nine.addEventListener('click', function(){
+    updateDisplay('9');
+      
+})
+
+eight.addEventListener('click', function(){
+    updateDisplay('8');
+
+})
+
+seven.addEventListener('click', function(){
+    updateDisplay('7');
+})
+
+six.addEventListener('click', function(){
+    updateDisplay('6');
+
+})
+
+five.addEventListener('click', function(){
+    updateDisplay('5');
+
+})
+
+four.addEventListener('click', function(){
+    updateDisplay('4');
+
+})
+
+three.addEventListener('click', function(){
+    updateDisplay('3');
+
+})
+
+two.addEventListener('click', function(){
+    updateDisplay('2');
+})
+
+one.addEventListener('click', function(){
+    updateDisplay('1');
+})
+
+zero.addEventListener('click', function(){
+    updateDisplay('0');
+})
+
+
+plus.addEventListener('click', function(){
+    operator = '+'
+    updateDisplay(operator);
+})
+
+quotient.addEventListener('click', function(){
+    operator = '/'
+    updateDisplay(operator);
+})
+
+
+minus.addEventListener('click', function(){
+    operator = '-'
+    updateDisplay(operator);
+
+})
+
+product.addEventListener('click', function(){
+    operator = '*'
+    updateDisplay(operator);
+})
+
+
+clear.addEventListener('click', function() {
+    displayValue = '';
+    output.innerText = '';
+
+})
+
+del.addEventListener('click', function() {
+
+    let currentText = output.textContent;
+    if (currentText.length > 0) {
+        output.textContent = currentText.slice(0,-1);
+        displayValue =  output.textContent;
+    }
+})
+
+equal.addEventListener('click', function(){
+   
+  let expression = displayValue.split(operator);
+  console.log(expression);
+
+  firstNum = parseFloat(expression[0]);
+  secondNum = parseFloat(expression[1]);
+
+  let result = operate(operator, firstNum, secondNum);
+  output.textContent = result;
+
+  displayValue = '';
+
+    
+})
+
+
+
+
